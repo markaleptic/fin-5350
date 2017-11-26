8507f6e89d08e925b1888f1615748c26ad7b85af
Mark Allred <markjallred@gmail.com>
2017-11-25 18:37:24 -0700

    Update OptionData with spaces

--- Notebooks/Option Pricing Engine/OptionData.py
+++ Notebooks/Option Pricing Engine/OptionData.py
@@ -1,23 +1,22 @@
 class OptionData(object):
-    def __init__(self, rate, spot, volatility, dividend):
-        self.rate = rate
-        self.spot = spot
-        self.volatility = volatility
-        self.dividend = dividend
-    # TODO - Define individual set methods
+  def __init__(self, rate, spot, volatility, dividend):
+    self.rate = rate
+    self.spot = spot
+    self.volatility = volatility
+    self.dividend = dividend
+    
 
-    def get_data(self):
-        return self.rate, self.spot, self.volatility, self.dividend
+  def get_data(self):
+    return self.rate, self.spot, self.volatility, self.dividend
 
-    def get_rate(self):
-        return self.rate
+  def get_rate(self):
+    return self.rate
 
-    def get_spot(self):
-        return self.spot
+  def get_spot(self):
+    return self.spot
 
-    def get_volatility(self):
-        return self.volatility
+  def get_volatility(self):
+    return self.volatility
 
-    def get_dividend(self):
-        return self.dividend
-
+  def get_dividend(self):
+    return self.dividend
